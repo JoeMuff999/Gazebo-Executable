6f89b2acca34fb45f386ada58e0b7971dbdf4487
Joey <joeymuffoletto10@gmail.com>
2019-11-22 01:10:52 -0600

    got decently far

---
+++ test.java
@@ -0,0 +1,109 @@
+
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import java.io.File;
+
+public class test
+{
+    static Document xmlDoc;
+    static Element root;
+    public static void main (String [] args)
+    {
+        writeXML(Integer.parseInt(args[1]));
+    }
+
+    public static void writeXML(int tBAmt)
+    {
+        try{
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            xmlDoc = dBuilder.newDocument();
+    
+            root = xmlDoc.createElement("launch");
+            xmlDoc.appendChild(root);
+
+            int[][] pos = new int[tBAmt][3];
+            int[][] attitude = new int[tBAmt][3];
+
+            writeTurtlebots(tBAmt, "String TURTLEBOT3_MODEL", pos, attitude);
+    
+            TransformerFactory tFF = TransformerFactory.newInstance();
+            Transformer tF = tFF.newTransformer();
+            tF.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+            DOMSource src = new DOMSource(xmlDoc);
+            String currPath = System.getProperty("user.dir");
+            StreamResult res = new StreamResult(new File(currPath + "/test.xml" ));
+            tF.transform(src, res);
+    
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        
+    }
+    /* TODO AMCL parameters --> http://wiki.ros.org/amcl#Parameters */
+    public static void writeTurtlebots(int amt, String TURTLEBOT3_MODEL, int[][] initialPositions, int[][] initialOrientations)
+    {
+        //standard xml format for tbModel 
+        //<arg name="model" default="$(env TURTLEBOT3_MODEL)" doc="model type [burger, waffle, waffle_pi]"/>
+
+        Element tBModel = xmlDoc.createElement("arg"); 
+        Attr doc = xmlDoc.createAttribute("doc");
+        doc.setValue("model type [burger, waffle, waffle_pi]");
+
+        Attr nameAttribute = xmlDoc.createAttribute("name");
+        Attr defaultAttribute = xmlDoc.createAttribute("default");
+    
+        nameAttribute.setValue("model");
+        defaultAttribute.setValue(TURTLEBOT3_MODEL);
+
+        tBModel.setAttributeNode(nameAttribute);
+        tBModel.setAttributeNode(defaultAttribute);
+        tBModel.setAttributeNode(doc);
+
+        
+        root.appendChild(tBModel);
+
+        //for each turtlebot, create model arg ,  create position arg, and create attitude arg
+        for(int i = 0; i < amt; i++)
+        {
+            //model arg
+            Attr name = xmlDoc.createAttribute("name");
+            name.setValue(i+1 + "_tb3");
+            Attr def = xmlDoc.createAttribute("default");
+            def.setValue("tb3_" + i);
+            Attr[] attrs = new Attr[]{name,def};
+            Element tb = createElementHelper("arg",attrs);
+            root.appendChild(tb);       
+            
+            //position arg
+
+        }
+
+        //for each turtlebot, set their default positions
+        
+
+    }
+
+    public static Element createElementHelper(String name, Attr[] attributes)
+    {
+        Element elem = xmlDoc.createElement(name);
+        for(Attr attr : attributes)
+        {
+            elem.setAttributeNode(attr);
+        }
+        return elem;
+    }
+}
