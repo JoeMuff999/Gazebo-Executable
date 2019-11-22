78f88ad809e1732a06c8d23fe6b681e16a72d4c8
undergrad <joeymuffoletto10@gmail.com>
2019-11-22 12:56:16 -0800

    added pos args

--- test.java
+++ test.java
@@ -29,7 +29,7 @@ public class test
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             xmlDoc = dBuilder.newDocument();
-    
+
             root = xmlDoc.createElement("launch");
             xmlDoc.appendChild(root);
 
@@ -37,7 +37,7 @@ public class test
             int[][] attitude = new int[tBAmt][3];
 
             writeTurtlebots(tBAmt, "String TURTLEBOT3_MODEL", pos, attitude);
-    
+
             TransformerFactory tFF = TransformerFactory.newInstance();
             Transformer tF = tFF.newTransformer();
             tF.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
@@ -45,27 +45,27 @@ public class test
             String currPath = System.getProperty("user.dir");
             StreamResult res = new StreamResult(new File(currPath + "/test.xml" ));
             tF.transform(src, res);
-    
+
         }
         catch(Exception e)
         {
             e.printStackTrace();
         }
-        
+
     }
     /* TODO AMCL parameters --> http://wiki.ros.org/amcl#Parameters */
     public static void writeTurtlebots(int amt, String TURTLEBOT3_MODEL, int[][] initialPositions, int[][] initialOrientations)
     {
-        //standard xml format for tbModel 
+        //standard xml format for tbModel
         //<arg name="model" default="$(env TURTLEBOT3_MODEL)" doc="model type [burger, waffle, waffle_pi]"/>
 
-        Element tBModel = xmlDoc.createElement("arg"); 
+        Element tBModel = xmlDoc.createElement("arg");
         Attr doc = xmlDoc.createAttribute("doc");
         doc.setValue("model type [burger, waffle, waffle_pi]");
 
         Attr nameAttribute = xmlDoc.createAttribute("name");
         Attr defaultAttribute = xmlDoc.createAttribute("default");
-    
+
         nameAttribute.setValue("model");
         defaultAttribute.setValue(TURTLEBOT3_MODEL);
 
@@ -73,26 +73,41 @@ public class test
         tBModel.setAttributeNode(defaultAttribute);
         tBModel.setAttributeNode(doc);
 
-        
+
         root.appendChild(tBModel);
 
         //for each turtlebot, create model arg ,  create position arg, and create attitude arg
         for(int i = 0; i < amt; i++)
         {
             //model arg
-            Attr name = xmlDoc.createAttribute("name");
-            name.setValue(i+1 + "_tb3");
-            Attr def = xmlDoc.createAttribute("default");
-            def.setValue("tb3_" + i);
+            Attr name = createAttributeHelper("name", i+1 + "_tb3");
+            Attr def = createAttributeHelper("default", "tb3_" + i);
             Attr[] attrs = new Attr[]{name,def};
-            Element tb = createElementHelper("arg",attrs);
-            root.appendChild(tb);       
-            
-            //position arg
-            
-        }
-        
+            Element argElem = createElementHelper("arg",attrs);
+            root.appendChild(argElem);
 
+            //position arg
+            String[] posStr = new String[]{"x","y","z"};
+            int index = 0;
+            for(String s : posStr)
+            {
+              name = createAttributeHelper("name", i+1 + "_tb3_" + s + "_pos");
+              def = createAttributeHelper("default", "" + initialPositions[i][index]);
+              argElem = createElementHelper("arg", new Attr[]{name,def});
+              root.appendChild(argElem);
+              index++;
+            }
+
+        }
+
+
+    }
+
+    public static Attr createAttributeHelper(String name, String value)
+    {
+      Attr attr = xmlDoc.createAttribute(name);
+      attr.setValue(value);
+      return attr;
     }
 
     public static Element createElementHelper(String name, Attr[] attributes)
